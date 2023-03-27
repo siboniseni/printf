6ecc7c5fbc2131e13fec0f2683ef37ee2b6d95cd
siboniseni <siboniseni.kasa@outlook.com>
2023-03-27 13:15:28 -0700

    My 2ND commit S

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
@@ -63,23 +63,5 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-}
-
-vi putchar
-
-#include <unistd.h>
-#include "main.h"
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
 
