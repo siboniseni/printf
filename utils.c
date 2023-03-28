448f01f725b182929d773614547d745f784a8ada
siboniseni <siboniseni.kasa@outlook.com>
2023-03-27 23:51:46 -0700

    compiling error fixed

--- utils.c
+++ utils.c
@@ -8,10 +8,10 @@
  */
 int is_printable(char c)
 {
-if (c >= 32 && c < 127)
-return (1);
+	if (c >= 32 && c < 127)
+		return (1);
 
-return (0);
+	return (0);
 }
 
 /**
@@ -23,18 +23,18 @@ return (0);
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-char map_to[] = "0123456789ABCDEF";
-/* The hexa format code is always 2 digits long */
-if (ascii_code < 0)
-ascii_code *= -1;
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-buffer[i++] = '\\';
-buffer[i++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-buffer[i++] = map_to[ascii_code / 16];
-buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
-return (3);
+	return (3);
 }
 
 /**
@@ -45,8 +45,8 @@ return (3);
  */
 int is_digit(char c)
 {
-if (c >= '0' && c <= '9')
-	return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
 	return (0);
 }
@@ -84,3 +84,4 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
+
